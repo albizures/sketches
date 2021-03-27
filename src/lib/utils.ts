1b44ee8334d8f766883094ba71acb559916342fb
Jose Albizures <jose@albizures.com>
2021-03-27 12:49:38 +0300

    add mirror function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,25 +1,40 @@
+import { scale } from '.';
+import { restore, save } from './sketch';
+
 const constrain = (value: number, start: number, stop: number) => {
-  return Math.max(Math.min(value, start), stop);
+	return Math.max(Math.min(value, start), stop);
 };
 
 const remap = (
-  value: number,
-  start1: number,
-  stop1: number,
-  start2: number,
-  stop2: number,
-  withinBounds: boolean
+	value: number,
+	start1: number,
+	stop1: number,
+	start2: number,
+	stop2: number,
+	withinBounds: boolean,
 ) => {
-  const newval =
-    ((value - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
-  if (!withinBounds) {
-    return newval;
-  }
-  if (start2 < stop2) {
-    return constrain(newval, start2, stop2);
-  } else {
-    return constrain(newval, stop2, start2);
-  }
+	const newval =
+		((value - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
+	if (!withinBounds) {
+		return newval;
+	}
+	if (start2 < stop2) {
+		return constrain(newval, start2, stop2);
+	} else {
+		return constrain(newval, stop2, start2);
+	}
+};
+
+export const mirror = (fn: () => void, vertical = true) => {
+	save();
+	fn();
+	if (vertical) {
+		scale(-1, 1);
+	} else {
+		scale(1, -1);
+	}
+	fn();
+	restore();
 };
 
 const noop = () => {};
